92d820fcdc938cc02ed961351a5f4920a99a1eab
dragosh -  azure <stefanescu.dragos@gmail.com>
2014-05-11 13:22:32 +0300

    producer edit done

--- PodcastAggregator/Controllers/ProducersController.cs
+++ PodcastAggregator/Controllers/ProducersController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
+using PodcastAggregator.Services;
 
 namespace PodcastAggregator.Controllers
 {
@@ -20,12 +23,48 @@ namespace PodcastAggregator.Controllers
         public ActionResult Create() { return View(new Producer()); }
 
         [HttpPost]
-        public ActionResult Create(Producer producer) {
+        public ActionResult Create(Producer producer)
+        {
             if (!ModelState.IsValid) return View(producer);
 
             _db.Producers.Save(producer);
 
             return RedirectToAction("Index");
         }
+
+        public ActionResult Edit(string id)
+        {
+            var producer = _db.Producers.AsQueryable().FirstOrDefault(x => x.Id == id);
+            if (producer == null)
+            {
+                LoggerService.Exception(new ApplicationException("No producer found for id:" + id));
+                return RedirectToAction("Index");
+            }
+            return View(producer);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string id, Producer model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var producer = _db.Producers.AsQueryable().FirstOrDefault(x => x.Id == id);
+            if (producer == null)
+            {
+                LoggerService.Exception(
+                    new ApplicationException( 
+                        string.Format( "No producer found to update for id:{0} \n with the following data to post: \n {1}", id, model.ToJson())
+                        ));
+                return RedirectToAction("Index");
+            }
+
+            _db.Producers.Save(model);
+                return RedirectToAction("Index");
+        }
+
+
     }
+
+
+
+
 }
